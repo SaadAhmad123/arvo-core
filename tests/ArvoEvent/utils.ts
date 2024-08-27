d32df8fe178b8227b1b61d972bd96b0a082c4efc
Saad Ahmad <saadkwi12@hotmail.com>
2024-08-27 17:25:10 +1000

    added typedoc and tested telemetry and did some refactoring

--- tests/ArvoEvent/utils.ts
+++ tests/ArvoEvent/utils.ts
@@ -5,6 +5,8 @@ import { ATTR_SERVICE_NAME } from '@opentelemetry/semantic-conventions';
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node'
 import { diag, DiagConsoleLogger, DiagLogLevel } from "@opentelemetry/api"
 
+diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO);
+
 // Console Logger
 export const telemetrySdk = new NodeSDK({
   resource: new Resource({
